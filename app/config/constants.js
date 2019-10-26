031416d17175429dd2fae7ec3baeeed11b9bc31d
P Lowe <perun@reagan.com>
2019-10-26 06:48:34 -0400

    removing keys

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,8 +1,8 @@
-export const FIREBASE_API_KEY = 'AIzaSyCcQygN5k26Y2wgdNef_bHy2_MAVUvfi4g'
-export const FIREBASE_AUTH_DOMAIN = 'capstone-d3e9d.firebaseapp.com'
-export const FIREBASE_DATABASE_URL = 'https://capstone-d3e9d.firebaseio.com/'
-export const FIREBASE_PROJECT_ID = 'capstone-d3e9d.web.app'
-export const FIREBASE_STORAGE_BUCKET = 'gs://capstone-d3e9d.appspot.com'
-export const FIREBASE_MESSAGING_SENDER_ID = '754778352950'
+export const FIREBASE_API_KEY = 
+export const FIREBASE_AUTH_DOMAIN = 
+export const FIREBASE_DATABASE_URL = 
+export const FIREBASE_PROJECT_ID = 
+export const FIREBASE_STORAGE_BUCKET = 
+export const FIREBASE_MESSAGING_SENDER_ID = 
 
-export const FACEBOOK_APP_ID = '763805317402589'
+export const FACEBOOK_APP_ID = 
